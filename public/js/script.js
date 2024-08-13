ee5d241f62543b5ffb571a97e7af20e8583736d2
Istiak <mhistiak2@gmail.com>
2024-08-13 15:49:17 +0600

    TopAppBar Scroll Event and Menu toggle event done

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,35 @@
 /*
  * Title: MindScribe
- * Description: Frontend js 
- * Author: Istiak Ahammad 
- * Date: 
+ * Description: Frontend js
+ * Author: Istiak Ahammad
+ * Date: 8/13/2024
  *
  */
+
+const topAppBar = document.querySelector("[data-top-app-bar]");
+let lastScrollPos = 0;
+
+/**
+ * Attaches even listener to the window scroll event, toggling classes on the top app bar based on scroll position.
+ **/
+window.addEventListener("scroll", (event) => {
+  topAppBar.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+  topAppBar.classList[
+    window.scrollY > lastScrollPos && window.scrollY > 50 ? "add" : "remove"
+  ]("hide");
+  lastScrollPos = window.scrollY;
+});
+
+/**
+ *  Toggle Menu
+ **/
+const menuWrappers = document.querySelectorAll("[data-menu-wrapper]");
+
+menuWrappers?.forEach((menuWrapper) => {
+  const menuToggler = menuWrapper.querySelector("[data-menu-toggler]");
+  const menu = menuWrapper.querySelector("[data-menu]");
+
+  menuToggler.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+});
