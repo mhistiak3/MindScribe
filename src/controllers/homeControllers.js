79b14b889dc1bae6c66cffa86a041eaa2497398b
Istiak <mhistiak2@gmail.com>
2024-08-22 17:03:36 +0600

    fetch all latest blog from mongodb and send them to ejs template

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -6,14 +6,28 @@
  *
  */
 
+const Blog = require("../models/blogModel");
+
 const renderHome = async (req, res, next) => {
   try {
+    // Retriving blog from the database, selecting specific fields adn populating 'owner' field
+    const latestBlog = await Blog.find()
+      .select("banner title createdAt readingTime reaction totalBookmark")
+      .populate({
+        path: "owner",
+        select: "name username profilePhoto",
+      }).sort({
+        cretedAt:"desc"
+      })
+
     res.render("./pages/home", {
       sessionUser: req.session.user,
       route: req.originalUrl,
+      latestBlog,
     });
   } catch (error) {
     console.log(error.message);
+    throw error;
   }
 };
 
